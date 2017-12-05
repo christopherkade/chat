510d4f3096217c25ac541cd7e9104c4c49dedf3c
christopher <christopher.kade@epitech.eu>
2017-12-05 18:20:50 +0000

    Switched from Material Design lite to Bulma Removed image sharing feature Updated Angular code Other tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { MatSnackBar } from '@angular/material';
 import * as firebase from 'firebase';
 
 const LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
@@ -11,23 +11,27 @@ const PROFILE_PLACEHOLDER_IMAGE_URL = '/assets/images/profile_placeholder.png';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   user: Observable<firebase.User>;
   currentUser: firebase.User;
   messages: FirebaseListObservable<any>;
   profilePicStyles: {};
-  topics = '';
   value = '';
+  messageForm : FormGroup;
 
-  constructor(public db: AngularFireDatabase, public afAuth: AngularFireAuth, public snackBar: MatSnackBar) {
+  constructor(public db: AngularFireDatabase, public afAuth: AngularFireAuth, private fb: FormBuilder) {
+    this.messageForm = fb.group({
+       'message': [null, Validators.required]
+     });
+
     this.user = afAuth.authState;
     this.user.subscribe((user: firebase.User) => {
-      console.log(user);
       this.currentUser = user;
 
-      if (user) { // User is signed in!
+      // User is signed in!
+      if (user) {
         this.profilePicStyles = {
           'background-image':  `url(${this.currentUser.photoURL})`
         };
@@ -39,29 +43,6 @@ export class AppComponent {
           }
         });
         this.messages.subscribe((messages) => {
-          // Calculate list of recently discussed topics
-          const topicsMap = {};
-          const topics = [];
-          let hasEntities = false;
-          messages.forEach((message) => {
-            if (message.entities) {
-              for (let entity of message.entities) {
-                if (!topicsMap.hasOwnProperty(entity.name)) {
-                  topicsMap[entity.name] = 0
-                }
-                topicsMap[entity.name] += entity.salience;
-                hasEntities = true;
-              }
-            }
-          });
-          if (hasEntities) {
-            for (let name in topicsMap) {
-              topics.push({ name, score: topicsMap[name] });
-            }
-            topics.sort((a, b) => b.score - a.score);
-            this.topics = topics.map((topic) => topic.name).join(', ');
-          }
-
           // Make sure new message scroll into view
           setTimeout(() => {
             const messageList = document.getElementById('messages');
@@ -76,7 +57,6 @@ export class AppComponent {
         this.profilePicStyles = {
           'background-image':  PROFILE_PLACEHOLDER_IMAGE_URL
         };
-        this.topics = '';
       }
     });
   }
@@ -86,12 +66,8 @@ export class AppComponent {
   }
 
   logout() {
+    this.messages = null;
     this.afAuth.auth.signOut();
-  }
-
-  // TODO: Refactor into text message form component
-  update(value: string) {
-    this.value = value;
   }
 
   // Returns true if user is signed-in. Otherwise false and displays a message.
@@ -101,20 +77,10 @@ export class AppComponent {
       return true;
     }
 
-    this.snackBar
-      .open('You must sign-in first', 'Sign in', {
-        duration: 5000
-      })
-      .onAction()
-      .subscribe(() => this.login());
-
     return false;
   };
 
-  // TODO: Refactor into text message form component
-  saveMessage(event: any, el: HTMLInputElement) {
-    event.preventDefault();
-
+  saveMessage() {
     if (this.value && this.checkSignedInWithMessage()) {
       // Add a new message entry to the Firebase Database.
       const messages = this.db.list('/messages');
@@ -124,63 +90,8 @@ export class AppComponent {
         photoUrl: this.currentUser.photoURL || PROFILE_PLACEHOLDER_IMAGE_URL
       }).then(() => {
         // Clear message text field and SEND button state.
-        el.value = '';
+        this.value = '';
       }).catch((err) => {
-        this.snackBar.open('Error writing new message to Firebase Database.', null, {
-          duration: 5000
-        });
-        console.error(err);
-      });
-    }
-  }
-
-  // TODO: Refactor into image message form component
-  saveImageMessage(event: any) {
-    event.preventDefault();
-    const file = event.target.files[0];
-
-    // Clear the selection in the file picker input.
-    const imageForm = <HTMLFormElement>document.getElementById('image-form');
-    imageForm.reset();
-
-    // Check if the file is an image.
-    if (!file.type.match('image.*')) {
-      this.snackBar.open('You can only share images', null, {
-        duration: 5000
-      });
-      return;
-    }
-
-    // Check if the user is signed-in
-    if (this.checkSignedInWithMessage()) {
-
-      // We add a message with a loading icon that will get updated with the shared image.
-      const messages = this.db.list('/messages');
-      messages.push({
-        name: this.currentUser.displayName,
-        imageUrl: LOADING_IMAGE_URL,
-        photoUrl: this.currentUser.photoURL || PROFILE_PLACEHOLDER_IMAGE_URL
-      }).then((data) => {
-        // Upload the image to Cloud Storage.
-        const filePath = `${this.currentUser.uid}/${data.key}/${file.name}`;
-        return firebase.storage().ref(filePath).put(file)
-          .then((snapshot) => {
-            // Get the file's Storage URI and update the chat message placeholder.
-            const fullPath = snapshot.metadata.fullPath;
-            const imageUrl = firebase.storage().ref(fullPath).toString();
-            return firebase.storage().refFromURL(imageUrl).getMetadata();
-          }).then((metadata) => {
-            // TODO: Instead of saving the download URL, save the GCS URI and
-            //       dynamically load the download URL when displaying the image
-            //       message.
-            return data.update({
-              imageUrl: metadata.downloadURLs[0]
-            });
-          });
-      }).catch((err) => {
-        this.snackBar.open('There was an error uploading a file to Cloud Storage.', null, {
-          duration: 5000
-        });
         console.error(err);
       });
     }
@@ -197,7 +108,6 @@ export class AppComponent {
     return firebase.messaging().getToken()
       .then((currentToken) => {
         if (currentToken) {
-          console.log('Got FCM device token:', currentToken);
           // Save the Device Token to the datastore.
           firebase.database()
             .ref('/fcmTokens')
@@ -205,27 +115,22 @@ export class AppComponent {
             .set(this.currentUser.uid);
         } else {
           // Need to request permissions to show notifications.
-          return this.requestNotificationsPermissions();
+          // TODO: Add Notifications
+          // return this.requestNotificationsPermissions();
         }
       }).catch((err) => {
-        this.snackBar.open('Unable to get messaging token.', null, {
-          duration: 5000
-        });
         console.error(err);
       });
   };
 
   // Requests permissions to show notifications.
-  requestNotificationsPermissions() {
-    console.log('Requesting notifications permission...');
-    return firebase.messaging().requestPermission()
-      // Notification permission granted.
-      .then(() => this.saveMessagingDeviceToken())
-      .catch((err) => {
-        this.snackBar.open('Unable to get permission to notify.', null, {
-          duration: 5000
-        });
-        console.error(err);
-      });
-  };
+  // requestNotificationsPermissions() {
+  //   console.log('Requesting notifications permission...');
+  //   return firebase.messaging().requestPermission()
+  //     // Notification permission granted.
+  //     .then(() => this.saveMessagingDeviceToken())
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // };
 }
