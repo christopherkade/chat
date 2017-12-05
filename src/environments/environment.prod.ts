fa36457e751ac213b7119d490045f06d223686d6
christopher <christopher.kade@epitech.eu>
2017-12-05 16:37:35 +0000

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDIporHDDONHfiavfq9Sd5l2Upvuk9_JEk",
+    authDomain: "kade-chat.firebaseapp.com",
+    databaseURL: "https://kade-chat.firebaseio.com",
+    projectId: "kade-chat",
+    storageBucket: "",
+    messagingSenderId: "42634767407"
+  }
+};
