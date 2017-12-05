7c54b9a6dd1af66389b35ab8c77fb7730ec036ef
christopher <christopher.kade@epitech.eu>
2017-12-05 16:59:31 +0000

    Updated authorized domain values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: "AIzaSyDIporHDDONHfiavfq9Sd5l2Upvuk9_JEk",
-    authDomain: "chat.christopherkade.com",
+    authDomain: "kade-chat.firebaseapp.com",
     databaseURL: "https://kade-chat.firebaseio.com",
     projectId: "kade-chat",
     storageBucket: "",
